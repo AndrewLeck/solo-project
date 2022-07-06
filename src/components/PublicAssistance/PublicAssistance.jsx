f609240c6cd751e4ead06a6ee513082d4b9e6d21
Andrew Leck <Aleck360@gmail.com>
2022-07-06 14:24:25 -0500

    Created buttons inside public assistance page that will like the user to food shelves, homeless shelters, and clothing options

--- src/components/PublicAssistance/PublicAssistance.jsx
+++ src/components/PublicAssistance/PublicAssistance.jsx
@@ -1,9 +1,23 @@
+import { Link } from 'react-router-dom';
 function PublicAssistance(){
     return(
         <>
-        <h1>Public Assistance Page</h1>
+            <h1>Public Assistance Page</h1>
+                <div>
+                    <Link to="/FoodShelves">
+                        <button> Food Shelves </button>
+                    </Link>
+                    <Link to="/HomelessShelters">
+                        <button> Homeless Shelters </button>
+                    </Link>
+                    <Link to="/ClothingOptions">
+                        <button> Clothing Options </button>
+                    </Link>
+                </div>
             <div>
+                <p>
                 image will go here
+                </p>
             </div>
             <div>
                 <button>Go home</button>

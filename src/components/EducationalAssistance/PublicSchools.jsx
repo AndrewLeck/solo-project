6dbfb0c201c8e6b0f50b6acbb4a7206b414532ab
Andrew Leck <Aleck360@gmail.com>
2022-07-11 12:30:08 -0500

    appending the correct data in my educational page

--- src/components/EducationalAssistance/PublicSchools.jsx
+++ src/components/EducationalAssistance/PublicSchools.jsx
@@ -1,7 +1,19 @@
 import { Link } from 'react-router-dom';
+import { useEffect } from 'react';
+import {useSelector, useDispatch} from 'react-redux';
 
 function PublicSchools(){
- 
+
+    const resources = useSelector((store) => store.resources);
+    const dispatch = useDispatch();
+
+    // on page load run this action call (FETCH_RESOURCES)
+    useEffect(() =>{
+        dispatch({
+            type: "FETCH_RESOURCES"
+        })
+      },[])
+
     return(
         <>
          <h1>Public Schools</h1>
@@ -19,35 +31,37 @@ function PublicSchools(){
             <div>
                 <header> Name </header>
               <ul>
-                  <li>
-                      This is where I will map through my array and append the name
-                  </li>
-                  <li>
-                      This is where I will map through my array and append the name
-                  </li>
-                  <li>
-                      This is where I will map through my array and append the name
-                  </li>
-                  <li>
-                      This is where I will map through my array and append the name
-                  </li>
+                  {resources.map((resource) => {
+                      if( resource.sub_category_id === 8){
+                        return(
+                            <li key={resource.id}>
+                                {resource.name}
+                            </li>
+                        )
+                      } 
+                  })}
               </ul>
             </div>
             <div>
             <header>Additional Resources</header>
               <ul>
-                  <li>
-                      This is where I will map through my array and append Additional Resources
-                  </li>
-                  <li>
-                      This is where I will map through my array and append Additional Resources
-                  </li>
-                  <li>
-                      This is where I will map through my array and append Additional Resources
-                  </li>
-                  <li>
-                      This is where I will map through my array and append Additional Resources
-                  </li>
+              {resources.map((resource) => {
+                     if( resource.sub_category_id === 8){
+                        return(
+                            <li key={resource.id}>
+                               <div>
+                                   Address: {resource.location}
+                               </div>
+                               <div>
+                                    Phone: {resource.phone}
+                               </div>                
+                               <div>
+                                   link: <a href={resource.link} target="_blank">{resource.link}</a>
+                               </div>
+                            </li>
+                        )
+                     }
+                 })}
               </ul>
             </div>
             <div>
